1944eb090e8ee17e294382825b0b75352ffb0f58
Lukas Elmer <lukas.elmer@renuo.ch>
2016-09-03 10:13:53 +0200

    add basic specs

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   description = `echo "\`git symbolic-ref HEAD 2> /dev/null | cut -b 12-\`-\`git log --pretty=format:\"%h\" -1\`"`
-  VERSION = "Two Griffins: #{description[0..-2]}"
+  VERSION = "Two Griffins: #{description[0..-2]}".freeze
 
   def bet_request(_game_state)
     1
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/player'
+require_relative 'mocks/game_state_mock'
+
+RSpec.describe Player do
+  describe 'bet_request' do
+    it 'returns a valid value' do
+      player = Player.new
+
+      1000.times do
+        result = player.bet_request(GameStateMock.new.for_bet_request)
+        expect(result).to eq(0)
+      end
+    end
+  end
+
+  describe 'showdown' do
+    it 'returns a valid value' do
+      player = Player.new
+
+      1000.times do
+        result = player.showdown(GameStateMock.new.for_showdown)
+        expect(result).to be_nil
+      end
+    end
+  end
+end
